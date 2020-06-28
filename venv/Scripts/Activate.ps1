d571be156be9a153e530a4c163b90c5de5674ab1
YarinBS <yarin10121@gmail.com>
2020-06-28 21:50:27 +0300

    database.py added

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Roi\PycharmProjects\IMDB_Cross_Validation\venv"
+$env:VIRTUAL_ENV="C:\Users\Yarin\PycharmProjects\IMDB_Cross_Validation\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
