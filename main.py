022ab7c56424b1b8cfab86f9c3a659f43748af9b
YarinBS <yarin10121@gmail.com>
2020-06-29 17:29:33 +0300

    Accuracy, Precision & Recall

--- main.py
+++ main.py
@@ -4,14 +4,50 @@ from algorithm_runner import AlgorithmRunner
 from sklearn.neighbors import KNeighborsClassifier
 
 
+def data_evaluation(true_label_list, predicted_label_list):
+    tp, tn, fp, fn = 0, 0, 0, 0
+    for i in range(len(true_label_list)):
+        if true_label_list[i] == predicted_label_list[i]:
+            if true_label_list[i] == 1:
+                tp += 1
+            else:
+                tn += 1
+        else:
+            if true_label_list[i] == 1:
+                fn += 1
+            else:
+                fp += 1
+    return {'tp': tp, 'tn': tn, 'fp': fp, 'fn': fn}
+
+
+def precision(tp, fp):
+    return (tp / (tp + fp))
+
+
+def recall(tp, fn):
+    return (tp / (tp + fn))
+
+def calc_accuracy(test_set, classifier):
+    correct = 0.0
+    total = len(test_set.keys())
+    for key in test_set:
+        real = test_set[key][-1]
+        predicted = classifier.predict(test_set[key][0:-1])
+        if real == predicted:
+            correct += 1.0
+    return correct / total
+
+
 def main(argv):
     path = "movie_metadata.csv"
     movies_data = Data(path)
     movies_data.preprocess()
     fold1, fold2, fold3, fold4, fold5 = movies_data.split_to_k_Folds()
     folds = [fold1, fold2, fold3, fold4, fold5]
-    knn_classifier = KNeighborsClassifier(n_neighbors=5)
+    knn_classifier = KNeighborsClassifier(n_neighbors=10)
     Knn_algo = AlgorithmRunner(knn_classifier)
+    precision_counter = 0
+    recall_counter = 0
     for fold in folds:
         train_bin = []
         test_bin = []
@@ -24,7 +60,10 @@ def main(argv):
                 test_bin.append(movies_data.labels[i])
         Knn_algo.fit(train_df, train_bin)
         knn_predict = Knn_algo.predict(test_df)
-        print (knn_predict)
+        data_eval_dict = data_evaluation(test_bin, knn_predict)
+        precision_counter += precision(data_eval_dict['tp'], data_eval_dict['fp'])
+        recall_counter += recall(data_eval_dict['tp'], data_eval_dict['fn'])
+    print(f'Precision:{precision_counter / 5}, Recall: {recall_counter / 5}, Accuracy: {}')
 
 
 if __name__ == '__main__':
