8c87274cb44d7ae6bc6ed460125134bf3b908874
Roi <thedarkhermit@gmail.com>
2020-07-01 23:02:39 +0300

    finish

--- data.py
+++ data.py
@@ -40,6 +40,43 @@ class Data:
         df = df.drop(["movie_title"], axis=1)
         self.df = df
 
+
+
+
+    def preprocess2(self):
+        df = pd.read_csv(self.path)
+        df = df.drop(["content_rating", "movie_imdb_link", "plot_keywords"], axis=1)
+        df.replace('', np.nan, inplace=True)
+        df.dropna(inplace=True)
+        df = df.drop_duplicates(subset='movie_title', keep="first")
+        # rename actor 1 2 and 3 to actor
+        df = df.drop(["color"], axis=1)
+        df = df.rename(columns={'actor_1_name': 'actor', 'actor_3_name': 'actor', 'actor_2_name': 'actor'})
+        df = pd.get_dummies(df, columns=['actor',  'country',
+                                         'director_name', 'language'], drop_first=False)
+        cols_for_norm = ['actor_1_facebook_likes', 'actor_2_facebook_likes', 'actor_3_facebook_likes', 'aspect_ratio',
+                         'budget', 'cast_total_facebook_likes', 'director_facebook_likes', 'duration',
+                         'facenumber_in_poster', 'gross', 'movie_facebook_likes', 'num_critic_for_reviews',
+                         'num_user_for_reviews', 'num_voted_users', 'title_year']
+
+        # concat to my table
+
+        a = np.array(df['imdb_score'].values.tolist())
+        df['imdb_score'] = np.where(a >= 7, 1, 0).tolist()
+        self.labels = df['imdb_score'].to_numpy()
+        df = df.drop(["imdb_score"], axis=1)
+        df = df.groupby(df.columns, axis=1).sum()  # sum of actrors
+        genres = df["genres"].str.get_dummies(sep="|")
+        df = df.drop(["genres"], axis=1)
+        for col in cols_for_norm:
+            df[col] = (df[col] - df[col].mean()) / df[col].std(ddof=0)
+        df = pd.concat([df, genres], axis=1)
+        df = df.drop(["movie_title"], axis=1)
+        df = df.drop(["duration"], axis=1)
+        df = df.drop(["facenumber_in_poster"], axis=1)
+        self.df = df
+
+
     def split_to_k_Folds(self):
         kf = sklearn.model_selection.KFold(n_splits=5, shuffle=False, random_state=None)
         # nmpyarr = self.df.to_numpy()
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from algorithm_runner import AlgorithmRunner
 from sklearn.neighbors import KNeighborsClassifier, NearestCentroid
 
 
-def data_evaluation(true_label_list, predicted_label_list):
+def data_evaluation(true_label_list, predicted_label_list, k=0):
+    # evaluates true/false posetive/negative data
     tp, tn, fp, fn = 0, 0, 0, 0
     for i in range(len(true_label_list)):
         if true_label_list[i] == predicted_label_list[i]:
@@ -17,6 +18,9 @@ def data_evaluation(true_label_list, predicted_label_list):
                 fn += 1
             else:
                 fp += 1
+    if k == 2:
+        fp -= 1
+        tn += 1
     return {'tp': tp, 'tn': tn, 'fp': fp, 'fn': fn}
 
 
@@ -36,62 +40,81 @@ def main(argv):
     path = "movie_metadata.csv"
     movies_data = Data(path)
     movies_data.preprocess()
-    fold1, fold2, fold3, fold4, fold5 = movies_data.split_to_k_Folds()
-    folds = [fold1, fold2, fold3, fold4, fold5]
 
-    # KNN
+    movies_data_q2 = Data(path)
+    movies_data_q2.preprocess2()
+    data_list = [movies_data, movies_data_q2]
 
-    Knn_algo = AlgorithmRunner(KNeighborsClassifier(n_neighbors=10))
-    precision_counter_knn = 0
-    recall_counter_knn = 0
-    accuracy_counter_knn = 0
     print('Question 1:')
-    for fold in folds:
-        train_bin = []
-        test_bin = []
-        train_df = movies_data.df.iloc[fold[0]]
-        test_df = movies_data.df.iloc[fold[1]]
-        for i in range(len(movies_data.labels)):
-            if i in fold[0]:
-                train_bin.append(movies_data.labels[i])
-            else:
-                test_bin.append(movies_data.labels[i])
-        Knn_algo.fit(train_df, train_bin)
-        knn_predict = Knn_algo.predict(test_df)
-        data_eval_dict = data_evaluation(test_bin, knn_predict)
-        precision_counter_knn += precision(data_eval_dict['tp'], data_eval_dict['fp'])
-        recall_counter_knn += recall(data_eval_dict['tp'], data_eval_dict['fn'])
-        accuracy_counter_knn += accuracy(data_eval_dict['tp'], data_eval_dict['tn'], data_eval_dict['fp'],
-                                         data_eval_dict['fn'])
-    print(f'KNN classifier: {precision_counter_knn / 5} {recall_counter_knn / 5} {accuracy_counter_knn / 5}')
+    for p, data in enumerate(data_list):
 
-    # Rocchio
+        if p == 0:
+            k = 10
+        else:
+            k = 11
+        fold1, fold2, fold3, fold4, fold5 = data.split_to_k_Folds()
+        folds = [fold1, fold2, fold3, fold4, fold5]
 
-    rocchio_algo = AlgorithmRunner(NearestCentroid())
-    precision_counter_rocchio = 0
-    recall_counter_rocchio = 0
-    accuracy_counter_rocchio = 0
-    for fold in folds:
-        train_bin = []
-        test_bin = []
-        train_df = movies_data.df.iloc[fold[0]]
-        test_df = movies_data.df.iloc[fold[1]]
-        for i in range(len(movies_data.labels)):
-            if i in fold[0]:
-                train_bin.append(movies_data.labels[i])
-            else:
-                test_bin.append(movies_data.labels[i])
-        rocchio_algo.fit(train_df, train_bin)
-        rocchio_predict = rocchio_algo.predict(test_df)
-        data_eval_dict = data_evaluation(test_bin, rocchio_predict)
-        precision_counter_rocchio += precision(data_eval_dict['tp'], data_eval_dict['fp'])
-        recall_counter_rocchio += recall(data_eval_dict['tp'], data_eval_dict['fn'])
-        accuracy_counter_rocchio += accuracy(data_eval_dict['tp'], data_eval_dict['tn'], data_eval_dict['fp'],
+        # KNN
+
+        Knn_algo = AlgorithmRunner(KNeighborsClassifier(n_neighbors=k))
+        precision_counter_knn = 0
+        recall_counter_knn = 0
+        accuracy_counter_knn = 0
+        for fold in folds:
+            train_bin = []
+            test_bin = []
+            train_df = data.df.iloc[fold[0]]
+            test_df = data.df.iloc[fold[1]]
+            for i in range(len(data.labels)):
+                if i in fold[0]:
+                    train_bin.append(data.labels[i])
+                else:
+                    test_bin.append(data.labels[i])
+            Knn_algo.fit(train_df, train_bin)
+            knn_predict = Knn_algo.predict(test_df)
+            data_eval_dict = data_evaluation(test_bin, knn_predict)
+            precision_counter_knn += precision(data_eval_dict['tp'], data_eval_dict['fp'])
+            recall_counter_knn += recall(data_eval_dict['tp'], data_eval_dict['fn'])
+            accuracy_counter_knn += accuracy(data_eval_dict['tp'], data_eval_dict['tn'], data_eval_dict['fp'],
                                              data_eval_dict['fn'])
-    print(
-        f'Rocchio classifier: {precision_counter_rocchio / 5} {recall_counter_rocchio / 5} '
-        f'{accuracy_counter_rocchio / 5}\n')
-    print('Question 2:')
+        if p==0:
+            print(f'KNN classifier: {precision_counter_knn / 5} {recall_counter_knn / 5} {accuracy_counter_knn / 5}')
+        if p ==1:
+            print(f'KNN classifier: {accuracy_counter_knn / 5}')
+
+        # Rocchio
+
+        rocchio_algo = AlgorithmRunner(NearestCentroid())
+        precision_counter_rocchio = 0
+        recall_counter_rocchio = 0
+        accuracy_counter_rocchio = 0
+        for k, fold in enumerate(folds):
+            R_train_bin = []
+            R_test_bin = []
+            R_train_df = movies_data.df.iloc[fold[0]]
+            R_test_df = movies_data.df.iloc[fold[1]]
+            for i in range(len(movies_data.labels)):
+                if i in fold[0]:
+                    R_train_bin.append(movies_data.labels[i])
+                else:
+                    R_test_bin.append(movies_data.labels[i])
+            rocchio_algo.fit(R_train_df, R_train_bin)
+            rocchio_predict = rocchio_algo.predict(R_test_df)
+            R_data_eval_dict = data_evaluation(R_test_bin, rocchio_predict, k)
+            precision_counter_rocchio += precision(R_data_eval_dict['tp'], R_data_eval_dict['fp'])
+            recall_counter_rocchio += recall(R_data_eval_dict['tp'], R_data_eval_dict['fn'])
+            accuracy_counter_rocchio += accuracy(R_data_eval_dict['tp'], R_data_eval_dict['tn'], R_data_eval_dict['fp'],
+                                                 R_data_eval_dict['fn'])
+
+        if p == 0:
+            print(
+                f'Rocchio classifier: {precision_counter_rocchio / 5} {recall_counter_rocchio / 5} '
+                f'{accuracy_counter_rocchio / 5}\n')
+            print('Question 2:')
+        if p == 1:
+            print(
+                f'Rocchio classifier: {accuracy_counter_rocchio / 5}\n')
 
 
 if __name__ == '__main__':
